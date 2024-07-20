02eb166694daafc0312b67cf2f426a951e3f2322
Luis Roman <louisroman133@gmail.com>
2024-07-20 17:07:06 -0400

    "still fixing mistakes"

---
+++ src/Components/ErrorPage.js
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>404 ERROR: PAGE NOT FOUND</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Components/Temp.js
@@ -0,0 +1,9 @@
+const Temp = () => {
+    return(
+        <div>
+            Test
+        </div>
+    );
+};
+
+export default Temp;
