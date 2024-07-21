c29e4348699c523bf0c4f2de6d289a6825835244
Luis Roman <louisroman133@gmail.com>
2024-07-21 02:55:41 -0400

    Mobile view has been created. Deploy is ready for Desktop, Tablet, and Mobile.

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,26 +1,63 @@
 import { Outlet, Link } from "react-router-dom";
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-import logo_linear from "../photos/logo-linear.jpg"
-import "../Styles/Layout.css"
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import logo_linear from "../photos/logo-linear.jpg";
+import "../Styles/Layout.css";
 
 const Layout = () => {
+    const [isNavVisible, setIsNavVisible] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavVisible(!isNavVisible);
+    };
+
+    const handleNavClick = () => {
+        setIsNavVisible(false);
+    };
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
     return (
         <>
             <header className="header">
                 <div className="logo">
-                    <img id='logo-linear' src={logo_linear} />
+                    <img id="logo-linear" src={logo_linear} alt="Logo" />
                 </div>
-                <nav className="nav">
-                    <Link to="/">HOME</Link>
-                    <Link to="/menu">MENU</Link>
-                    <Link to="/events">EVENTS</Link>
-                    <Link to="/gallery">GALLERY</Link>
-                    <a className="fb" href='https://www.facebook.com/profile.php?id=100075934792972' target="_blank">
+                <nav className={`nav ${isNavVisible ? "nav-visible" : ""}`}>
+                    <Link to="/" onClick={handleNavClick}>HOME</Link>
+                    <Link to="/menu" onClick={handleNavClick}>MENU</Link>
+                    <Link to="/events" onClick={handleNavClick}>EVENTS</Link>
+                    <Link to="/gallery" onClick={handleNavClick}>GALLERY</Link>
+                    <a className="fb" onClick={handleNavClick} href='https://www.facebook.com/profile.php?id=100075934792972' target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faFacebook} size="lg" />
                     </a>
                 </nav>
+                <div 
+                    className="nav-toggle" 
+                    onClick={toggleNav}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    style={{
+                        cursor: 'pointer',
+                        padding: '10px',
+                        borderRadius: '5px'
+                    }}
+                >
+                    <FontAwesomeIcon 
+                        icon={faBars} 
+                        className="bars-icon" 
+                        style={{ color: isHovered ? '#00aced' : '#f4fff1' }} 
+                    />
+                </div>
             </header>
 
             <Outlet />
