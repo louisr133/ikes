209eca7692b1c7de082bc7dedc4a2ff3b7aa7d26
Luis Roman <louisroman133@gmail.com>
2024-07-21 20:56:47 -0400

    Small edits to text on Specials.js page.

--- src/Components/Specials.js
+++ src/Components/Specials.js
@@ -5,9 +5,9 @@ import Footer from './Footer';
 const specials = [
     { day: 'MONDAY', description: 'FREE POOL FROM 6PM - 12AM' },
     { day: 'TUESDAY', description: 'TAQUILLA TUESDAY, $1 OFF ALL DAY' },
-    { day: 'WEDNESDAY', description: 'THE QUEEN OF HEARTS DRAW AND WHISKEY WEDNESDAY ($1 OFF WELL WHISKEY)' },
-    { day: 'THURSDAY', description: '$2 OFF DOMESTIC BEERS AND A TEXAS HOLD \'EM TOURNAMENT' },
-    { day: 'FRIDAY', description: 'GIGI SWEET SOUNDS (DJ AND KARAOKE) @ 8PM' },
+    { day: 'WEDNESDAY', description: 'THE QUEEN OF HEARTS AND WHISKEY WEDNESDAY ($1 OFF WELL WHISKEY)' },
+    { day: 'THURSDAY', description: '$2 OFF DOMESTIC BEERS AND TEXAS HOLD \'EM' },
+    { day: 'FRIDAY', description: 'GIGI SWEET SOUNDS (KARAOKE) @ 8PM' },
     { day: 'SATURDAY', description: '$4.50 ANY FLAVOUR TEA SHOTS' },
     { day: 'SUNDAY', description: 'SUNDAY FUNDAY (NO SPECIALS)' },
 ];
